726cbaa5ec60dd7ff52fdd771bdabdf5c16ff3d4
Lotan <lotan92@gmail.com>
2022-04-24 10:44:32 +0300

    start building backend *game api *db service *logger service

---
+++ backend/api/song/game.controller.js
@@ -0,0 +1,3 @@
+const songService = require('./game.service.js');
+const logger = require('../../services/logger.service')
+
---
+++ backend/api/song/game.service.js
@@ -0,0 +1,3 @@
+const dbService = require('../../services/db.service')
+const logger = require('../../services/logger.service')
+
